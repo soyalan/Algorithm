afe71ae13a7378113d66998b3c2b594d12626106
soyalan <soyalan@naver.com>
2019-09-03 13:47:50 +0900

    09.03 TIL

--- SW_Expert_Academy/D4/1258.cpp
+++ SW_Expert_Academy/D4/1258.cpp
@@ -1,11 +1,67 @@
 #include <stdio.h>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main() {
+bool compare(pair<int, int> a, pair<int, int> b) {
+	if ((a.first * a.second) == (b.first * b.second))
+		return a.first < b.first;
+	return (a.first * a.second) < (b.first * b.second);
+}
 
-	cout << "hello" << endl;
+int main() {
+	int t;
+	cin >> t;
+	for (int k = 0; k < t; k++) {
+		int n;
+		int arr[100][100];
+		cin >> n;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				cin >> arr[i][j];
+			}
+		}
+		vector<pair<int, int>> v;
+		int width, height, x, y;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (arr[i][j]) {
+					width = 0;
+					height = 0;
+					x = j;
+					y = i;
+					while (arr[y][x]) {
+						x++;
+						width++;
+						if (x == n)break;
+					}
+					while (arr[y][x - 1]) {
+						y++;
+						height++;
+						if (y == n)break;
+					}
+					for (int w = i; w < y; w++) {
+						for (int h = j; h < x; h++) {
+							arr[w][h] = 0;
+						}
+					}
+					v.push_back(pair<int, int>(height, width));
+				}
+
+			}
+		}
+
+		sort(v.begin(), v.end(), compare);
+
+		vector<pair<int, int>>::iterator it = v.begin();
+		printf("#%d %d ", k + 1, v.size());
+		for (; it != v.end(); it++) {
+			printf("%d %d ", it->first, it->second);
+		}
+		printf("\n");
+	}
 
 	return 0;
 }
