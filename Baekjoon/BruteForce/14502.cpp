c4c0483a5aba90fd0f2a775dee0805674916e498
soyalan <soyalan@naver.com>
2019-09-04 13:53:18 +0900

    09.04 TIL

---
+++ Baekjoon/BruteForce/14502.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int width, height;
+int map[8][8];
+
+int main() {
+	scanf("%d %d", &height, &width);
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			scanf("%d", &map[i][j]);
+		}
+	}
+
+
+
+
+	return 0;
+}
