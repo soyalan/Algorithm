b417b53bb9aa9f852b3f18126f925ebf6c5525f7
soyalan <soyalan@naver.com>
2019-09-05 19:06:08 +0900

    09.05 TIL

--- AlgorithmStudy/1st/1920.cpp
+++ AlgorithmStudy/1st/1920.cpp
@@ -1,42 +1,31 @@
 #include <stdio.h>
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
 
-int arr[1001]; // 찾을 숫자
+int arr[100001]; // 찾을 숫자
 int N;
 
 int find(int n) {
-	int mid;
-	int first = 0;
-	int last = N - 1;
-	while (first <= last) {
-		mid = (first + last) / 2;
-		if (arr[mid] == n)return 1;
-		else if (arr[mid] > n) {
-			last = mid - 1;
-		}
-		else {
-			first = mid + 1;
-		}
-		if (mid >= N || mid < 0)break;
-	}
-
-	return 0;
+	if (binary_search(arr, arr + N, n))
+		return 1;
+	else
+		return 0;
 }
 
 int main() {
 	int M;
 	int b;
-	scanf("%d", &N);
+	scanf("%d ", &N);
 	for (int i = 0; i < N; i++) {
-		scanf("%d", &arr[i]);
+		scanf("%d ", &arr[i]);
 	}
 	sort(arr, arr + N);
 
-	scanf("%d", &M);
+	scanf("%d ", &M);
 	for (int i = 0; i < M; i++) {
-		scanf("%d", &b);
+		scanf("%d ", &b);
 		printf("%d\n", find(b));
 	}
 	return 0;
--- AlgorithmStudy/1st/README.md
+++ AlgorithmStudy/1st/README.md
@@ -19,3 +19,9 @@
 > 탐색 개수가 10만 x 10만이므로 완전 탐색 불가능
 >
 > 이진 탐색 (Binary Search) 사용
+>
+> <algorithm> 안에 있는 binary_serach 사용
+>
+> ```cpp
+> bool x = binary_search(v.begin(), v.end(), number);
+> ```
