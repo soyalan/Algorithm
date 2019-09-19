68c28be5166523fcaa580107043fc0b5cfeb19fb
soyalan <soyalan@naver.com>
2019-09-20 00:00:26 +0900

    change

--- AlgorithmStudy/2st/9663.cpp
+++ AlgorithmStudy/2st/9663.cpp
@@ -1,4 +1,3 @@
-/*
 #include <stdio.h>
 #include <iostream>
 
@@ -7,12 +6,19 @@ using namespace std;
 int N, cnt;
 int arr[15][15];
 
+void find(int y) {
 
+	for (int i = 0; i < N; i++) {
+
+	}
+}
 
 int main() {
+
 	cin >> N;
+
+	find(0);
 
 
 	return 0;
 }
-*/
